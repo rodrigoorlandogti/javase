f583c36afa5f4c1b9dbce22c34c2e6469cd44afc
Rodrigo Orlando <92063235+rodrigoorlandogti@users.noreply.github.com>
2022-08-25 17:01:40 -0300

    Criaçaõ da class: Principal (Uso da class Date, converção e formatação)

---
+++ src/modulo9/Principal.java
@@ -0,0 +1,18 @@
+package modulo9;
+
+import java.util.Date;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+
+		Date dt = new Date();
+		Date dt1 = new Date(1234567890L);
+		
+		System.out.println(dt);
+		System.out.println(dt1);
+
+	}
+
+}
